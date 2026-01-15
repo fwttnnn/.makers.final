2e32d0ea9c8afc31ce96250d309357e36a02e3ea
fwttnnn <lolywk@tutanota.com>
2026-01-15 18:08:56 +0700

    chore: hello world with mui

--- src/components/Movie/Input.tsx
+++ src/components/Movie/Input.tsx
@@ -3,15 +3,37 @@ import type { Root } from "@/stores"
 import type { State as Ticket } from "@/stores/tickets/reducer"
 import { increment, decrement } from "@/stores/tickets/actions"
 
+import Stack from "@mui/material/Stack"
+import Button from "@mui/material/Button"
+import Typography from "@mui/material/Typography"
+
 export default function App() {
   const dispatch = useDispatch()
   const ticket: Ticket = useSelector((r: Root) => r.tickets)
 
   return (
-    <div>
-      <p>count: {ticket.count}</p>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
-    </div>
+    <Stack spacing={2} alignItems="center">
+      <Typography variant="h6">
+        count: {ticket.count}
+      </Typography>
+
+      <Stack direction="row" spacing={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </Button>
+
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </Button>
+      </Stack>
+    </Stack>
   )
 }
